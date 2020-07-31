04981a217f9e08ac3b16e7a8a3ea42f4d9cb3b37
MonicaLuca <63265170+MonicaLuca@users.noreply.github.com>
2020-07-30 23:08:46 -0400

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+      if (l.includes(word)) {
+        return true;
+      }
     }
-}
+    const vertical = function(letters, word) {
+      // looping columns
+      for (let columns = 0; columns < letters[0].length; columns++) {
+        let newStr = '';
+        //loop rows
+        for (let rows = 0; rows < letters.length; rows++) {
+          newStr += letters[rows][columns];
+        }
+        if (newStr.includes(word)) {
+          return true;
+        }
+      }
+      return false;
+    };
+    return vertical(letters, word);
+  };
 
-module.exports = wordSearch
+  module.exports = wordSearch;
